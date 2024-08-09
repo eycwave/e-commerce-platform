e87db8a989a2895198744cca81bd2b7b7560a1c9
emreyuce <emreyuce000@gmail.com>
2024-08-09 10:19:15 +0300

    frontend-update

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -13,7 +13,9 @@ const Products = () => {
     name: '',
     description: '',
     price: '',
+    image: '',
   });
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -33,9 +35,7 @@ const Products = () => {
         const token = localStorage.getItem('token');
         if (token) {
           const decodedToken = jwtDecode(token);
-          console.log('Decoded Token:', decodedToken); // Token'in decode edilip edilmediğini kontrol etmek için log
-          setUserRole(decodedToken.role); // JWT token içinde rol bilgisi olduğunu varsayıyoruz
-          console.log('User Role:', decodedToken.role); // Rol bilgisini kontrol etmek için log
+          setUserRole(decodedToken.role);
         }
       } catch (error) {
         setError('Failed to decode token.');
@@ -50,17 +50,34 @@ const Products = () => {
   const handleAddProduct = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('/api/products/add', newProduct, {
+      const response = await axios.post('/api/products/add', newProduct, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
       });
-      setProducts([...products, newProduct]);
+      const addedProduct = response.data;
+      
+      setProducts([...products, addedProduct]);
       setNewProduct({ name: '', description: '', price: '', image: '' });
       setShowForm(false);
     } catch (error) {
       setError('Failed to add product.');
       console.error('Error adding product:', error);
+    }
+};
+
+
+  const handleDeleteProduct = async (id) => {
+    try {
+      await axios.delete(`/api/products/delete/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      setProducts(products.filter(product => product.id !== id));
+    } catch (error) {
+      setError('Failed to delete product.');
+      console.error('Error deleting product:', error);
     }
   };
 
@@ -69,85 +86,113 @@ const Products = () => {
     setNewProduct({ ...newProduct, [name]: value });
   };
 
+  const handleAddToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
 
   return (
     <div className="products-container">
-      <h1>Products</h1>
-      {userRole.includes('ROLE_ADMIN') && (
-        <div className="admin-button-container">
-          <button className="toggle-form" onClick={() => setShowForm(!showForm)}>
-            {showForm ? 'Cancel' : 'Add Product'}
-          </button>
-          {showForm && (
-            <form className="form-container" onSubmit={handleAddProduct}>
-              <div className="form-field">
-                <label>Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  value={newProduct.name}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="form-field">
-                <label>Description</label>
-                <input
-                  type="text"
-                  name="description"
-                  value={newProduct.description}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="form-field">
-                <label>Price</label>
-                <input
-                  type="number"
-                  name="price"
-                  value={newProduct.price}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="form-field">
-                <label>Image URL</label>
-                <input
-                  type="text"
-                  name="image"
-                  value={newProduct.image}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          )}
-        </div>
-      )}
+      <div className="products-header">
+        <h1>Products</h1>
+        {userRole.includes('ROLE_ADMIN') && (
+          <div className="admin-button-container">
+  <button
+    className="toggle-form"
+    onClick={() => setShowForm(!showForm)}
+  >
+    {showForm ? 'Cancel' : 'Add Product'}
+  </button>
+  {showForm &&  (
+    <form className="product-form-container" onSubmit={handleAddProduct}>
+      <div className="product-form-field">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={newProduct.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="product-form-field">
+        <label>Description</label>
+        <input
+          type="text"
+          name="description"
+          value={newProduct.description}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="product-form-field">
+        <label>Price</label>
+        <input
+          type="number"
+          name="price"
+          value={newProduct.price}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="product-form-field">
+        <label>Image URL</label>
+        <input
+          type="text"
+          name="image"
+          value={newProduct.image}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )}
+</div>
+
+        )}
+      </div>
+
       <div className="products-list">
         {products.map((product) => {
-          // Sayıya dönüştürme
           const price = parseFloat(product.price);
           return (
             <div key={product.id} className="product-item">
+              {userRole.includes('ROLE_ADMIN') && (
+                <button className="delete-button" onClick={() => handleDeleteProduct(product.id)}>
+                  &#10006;
+                </button>
+              )}
               <img src={product.image} alt={product.name} className="product-image" />
               <div className="product-details">
                 <h2 className="product-name">{product.name}</h2>
                 <p className="product-description">{product.description}</p>
                 <p className="product-price">${isNaN(price) ? 'N/A' : price.toFixed(2)}</p>
+                <button onClick={() => handleAddToCart(product)}>Buy</button>
               </div>
             </div>
           );
         })}
       </div>
+
+      <div className="cart-container">
+        <h2>Cart</h2>
+        {cart.length > 0 ? (
+          cart.map((item, index) => (
+            <div key={index} className="cart-item">
+              <p>{item.name}</p>
+              <p>${parseFloat(item.price).toFixed(2)}</p>
+            </div>
+          ))
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
+        <button className="create-order-button">Create Order</button>
+      </div>
     </div>
   );
 };
-
-  
-
 
 export default Products;
