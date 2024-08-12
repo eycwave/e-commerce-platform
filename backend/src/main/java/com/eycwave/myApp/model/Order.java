08a35a319f8c777869989fb9e81a78f4302bdcf2
emreyuce <emreyuce000@gmail.com>
2024-08-12 17:50:59 +0300

    backend-order added

---
+++ backend/src/main/java/com/eycwave/myApp/model/Order.java
@@ -0,0 +1,42 @@
+package com.eycwave.myApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+    private String uuid;
+    private String orderNumber;
+    private Timestamp orderDate;
+    private Double totalAmount;
+    private String orderStatus;
+
+    // Relations
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_product",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private List<Product> productList;
+
+}
