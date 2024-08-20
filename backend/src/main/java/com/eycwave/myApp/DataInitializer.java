27268930fc6400fbccfd18738e8443e922ccd304
emreyuce <emreyuce000@gmail.com>
2024-08-20 11:10:54 +0300

    backend-revised

--- backend/src/main/java/com/eycwave/myApp/DataInitializer.java
+++ backend/src/main/java/com/eycwave/myApp/DataInitializer.java
@@ -3,18 +3,14 @@ package com.eycwave.myApp;
 import com.eycwave.myApp.enums.Role;
 import com.eycwave.myApp.kafka.BotService;
 import com.eycwave.myApp.model.User;
-import com.eycwave.myApp.model.dto.OrderDto;
 import com.eycwave.myApp.repository.UserRepository;
 import com.eycwave.myApp.service.CartService;
-import com.eycwave.myApp.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Component
